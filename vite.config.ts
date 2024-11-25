269b0d536aeb8f0c4c098639b52f3ac2b2068e95
firdavs9777 <fmutalipov97@gmail.com>
2024-11-25 14:45:02 +0900

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';  // Import tailwindcss using import
 import autoprefixer from 'autoprefixer';  // Import autoprefixer using import
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/React_Google_Search_Location',
+  plugins: [react(), viteCompression()],
+  base: './',
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],  // Use the imported modules here
