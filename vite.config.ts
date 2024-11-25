714eb9531d6f24b7ab9e9af7a1b4a9ff19ea7a17
firdavs9777 <fmutalipov97@gmail.com>
2024-11-25 14:51:36 +0900

    Minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';  // Import tailwindcss using import
 import autoprefixer from 'autoprefixer';  // Import autoprefixer using import
-import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
+  plugins: [react()],
   base: './',
   css: {
     postcss: {
